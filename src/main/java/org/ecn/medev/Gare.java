8cdd7be667a26216b267e4f4bdf825253d97e567
Bricecrx <94597541+Bricecrx@users.noreply.github.com>
2022-11-17 11:30:20 +0100

    modif toString Gare

--- src/main/java/org/ecn/medev/Gare.java
+++ src/main/java/org/ecn/medev/Gare.java
@@ -17,7 +17,7 @@ public class Gare extends Achetable {
 
     @Override
     public String toString() {
-        if (proprietaire.getNom() != null) {
+        if (proprietaire != null) {
             return "Gare de" + this.getNom() + " (coût :" + this.getPrix() + ") - " + proprietaire.getNom();
         } else {
             return "Gare de" + this.getNom() + " (coût :" + this.getPrix() + ") - ";
