95e1152316ec2ae7819438cbe9011b2096807a02
Erwan QUINOT <erwan.quinot@gmail.com>
2022-11-17 09:20:58 +0100

    added spefication

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Joueur.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Joueur.java
@@ -5,26 +5,40 @@
 package com.mycompany.mavenproject1;
 
 /**
- *
+ * Classe Joueur
  * @author erwan
  */
 public class Joueur {
+    
+    /** Le nom du joueur. */
     private String nom;
+    /** La fortune du joueur. */
     private int fortune;
+    /** La position du joueur. */
     private int position;
     
+    /** Constructeur de Joueur.
+     * @param nom Nom du Joueur.
+     * @param fortune Fortune du Joueur.
+     * @param position Position du Joueur.
+    */
     Joueur(String nom, int fortune, int position) {
         this.nom = nom;
         this.fortune = fortune;
         this.position = position;
     }
     
+    /** Constructeur par defaut de Joueur. */
     Joueur() {
         this.nom = "Jacques";
         this.fortune = 100000;
         this.position = 0;       
     }
     
+    /** Paiement d'un autre joueur.
+     * @param cout cout du paiement.
+     * @param j Joueur a payer.
+    */    
     public void payer(int cout, Joueur j) {
         if(this.fortune > cout) {
             this.fortune -= cout;
@@ -33,12 +47,8 @@ public class Joueur {
         else {
             j.setFortune(j.getFortune() + this.fortune);
             this.fortune = 0;
-            finDePartie();
+            //finDePartie();
         }
-    }
-    
-    public void finDePartie() {
-        
     }
 
     public String getNom() {
