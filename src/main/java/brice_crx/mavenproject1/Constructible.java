65f28ddd76775614400e3ce08dba145be97e192c
Pierre <72155300+Pierre36@users.noreply.github.com>
2022-11-17 09:24:27 +0100

    Création de la classe Constructible

---
+++ src/main/java/brice_crx/mavenproject1/Constructible.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package brice_crx.mavenproject1;
+
+/**
+ * Classe constructible
+ * @author Pierre Baudet
+ */
+public class Constructible extends Achetable {
+    
+    private int nbMaisons;
+    private int nbHotels;
+    
+    public Constructible() {
+        this.nbMaisons = 0;
+        this.nbHotels = 0;
+    }
+
+    public Constructible(int nbMaisons, int nbHotels) {
+        this.nbMaisons = nbMaisons;
+        this.nbHotels = nbHotels;
+    }
+
+    public int getNbMaisons() {
+        return nbMaisons;
+    }
+
+    public int getNbHotels() {
+        return nbHotels;
+    }
+
+    public void setNbMaisons(int nbMaisons) {
+        this.nbMaisons = nbMaisons;
+    }
+
+    public void setNbHotels(int nbHotels) {
+        this.nbHotels = nbHotels;
+    }
+    
+    private void contruire(int nbNouvellesMaisons, int nbNouveauxHotels) {
+        this.nbMaisons += nbNouvellesMaisons;
+        this.nbHotels += nbNouveauxHotels;
+    }
+}
