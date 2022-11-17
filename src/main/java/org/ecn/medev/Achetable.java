e5230f24fddd8db42151c434e83ef5e77e3f553e
Monhza <nicolas.thevenot@live.fr>
2022-11-17 09:19:12 +0100

    Déplacement du fichier Achetable

---
+++ src/main/java/org/ecn/medev/Achetable.java
@@ -0,0 +1,19 @@
+package org.ecn.medev;
+
+public class Achetable extends Case{
+
+    public int prix;
+    public Joueur proprietaire;
+
+    public Achetable() {
+
+    }
+
+    public void acheter(){
+
+    }
+
+    public int CalculLoyer() {
+
+    }
+}
